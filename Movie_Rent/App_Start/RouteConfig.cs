b304c5f56065212c9d1ee5e94a1f5aee69922f9b
Matan Atiya <matan.atiya302@gmail.com>
2018-12-16 16:43:23 +0200

    Add files via upload

---
+++ Movie_Rent/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Movie_Rent
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                 name: "LoginPage",
+                 url: "",
+                 defaults: new { controller = "Login", action = "LoginPage", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                 name: "ReLoginPage",
+                 url: "LoginPage",
+                 defaults: new { controller = "Login", action = "LoginPage", id = UrlParameter.Optional }
+            );
+
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
